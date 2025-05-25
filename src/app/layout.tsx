d7458d71b2cd27b2dd0dad886d7070b889e6305a
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-05-25 10:32:45 -0400

    feat: wrap main content with PageWrapper component for improved layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import PageWrapper from "./components/PageWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,7 +46,9 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div className="min-h-screen flex flex-col">
           <Navbar />
-          <main className="flex-grow">{children}</main>
+          <main className="flex-grow">
+            <PageWrapper>{children}</PageWrapper>
+          </main>
           {/* Footer goes here in the future */}
         </div>
       </body>
